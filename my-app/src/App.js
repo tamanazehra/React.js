bc13d9cb5d3c696edc9237ad40ea96720d6a0c74
tamanazehra <zehratamana3@gmail.com>
2025-06-03 15:15:22 +0530

    Creating Profile-Card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,29 @@ function Welcome() {
   const name = "Tamana";
   return <h1>Hello, {name}! Welcome to React.</h1>;
 }
-function Body(){
-  return <p>This is the body of the application.</p>;
-}
-function Footer() {
-  return <p>© 2025 Tamana. All rights reserved.</p>;
+
+function ProfileCard({ name, bio, location  }) {
+  return <div className="info">
+    <h2>{name}</h2>
+    <p>{bio}</p>
+    <p>{location}</p>
+    </div>
 }
 
+function Footer(prop) {
+  return <p>© {prop.year} {prop.name} All rights reserved.</p>;
+}
 
 function App() {
   return (
     <div className="App">
       <Welcome />
-      <Body />
-      <Footer />
+      <ProfileCard 
+        name={"Tamana"}
+        bio={"A passionate developer."}
+        location={"Dhaka, Bangladesh"} />
+      <Footer year = {2025} name = {"Tamana"} />
+      
     </div>
   );
 }
